9765a71f194329450aa3dab6dbe86882592085f0
Etienne Rivard <etienne@kerzo.ca>
2024-11-16 09:44:26 -0500

    retroaction

---
+++ correction.md
@@ -0,0 +1,3 @@
+Beau travail!
+
+Il ne manquait que de retourner les cartes lorsque le jeu est terminé.
--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 import './App.css';
-import Cartes from './components/cartes.component'
+import Cartes from './components/cartes.component';
 
 function App() {
   // 20 coups max
@@ -10,10 +10,10 @@ function App() {
 
   // Si le nombre de coups augmente, on vérifie l'état de la partie
   useEffect(() => {
-    if(nombreCoups <= 0){
-      setPartieEnCours(false)
+    if (nombreCoups <= 0) {
+      setPartieEnCours(false);
     }
-  }, [nombreCoups])
+  }, [nombreCoups]);
 
   // Rafraîchir la page
   /** Emprunté de : https://upmostly.com/tutorials/how-to-refresh-a-page-or-component-in-react */
@@ -22,31 +22,27 @@ function App() {
   }
 
   // Je suis vraiment incertain de savoir comment on fait des if imbriqués ou des else if(...) en React, donc je m'excuse pour ces conditions
+  // ER: Ça aurait été plus élégant de faire une variable avec le message à afficher et de l'afficher à la fin
   return (
-    <div className='jeu'>
+    <div className="jeu">
       <h1>Jeu de mémoire</h1>
       <p>Nombre de coups : {nombreCoups}</p>
-      {
-        !partieEnCours && nombreCoups == 0 ?
-        (<p>Partie perdue!</p>) : (<></>)
-      }
-      {
-        !partieEnCours && nombreCoups > 0 ?
-        (<p>Partie gagnée!</p>) : (<></>)
-      }
-      {
-        !partieEnCours ?
+      {!partieEnCours && nombreCoups == 0 ? <p>Partie perdue!</p> : <></>}
+      {!partieEnCours && nombreCoups > 0 ? <p>Partie gagnée!</p> : <></>}
+      {!partieEnCours ? (
         <>
-        <button onClick={RelancerPartie}>Relancer la partie!</button>
+          <button onClick={RelancerPartie}>Relancer la partie!</button>
         </>
-        : <></>
-      }
+      ) : (
+        <></>
+      )}
       <br></br>
       <Cartes
         nombreEssais={nombreCoups}
         setNombreEssais={setNombreCoups}
         partieEnCours={partieEnCours}
-        setPartieEnCours={setPartieEnCours}/>
+        setPartieEnCours={setPartieEnCours}
+      />
     </div>
   );
 }
